e19c13bb6c9bcff9bbf597149d6e5276bdd5531c
rosalinda06 <49203776+rosalinda06@users.noreply.github.com>
2019-04-16 12:03:18 -0500

    Add files via upload

---
+++ While.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkgwhile;
+
+/**
+ *
+ * @author PC25-LAB02
+ */
+public class While {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int n=1;
+        while(n<=10){
+            System.out.println(n);
+            n=n+1;
+        }
+        
+                
+        
+    }
+    
+}
---
+++ While2.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package while2;
+
+/**
+ *
+ * @author PC25-LAB02
+ */
+public class While2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int n=15;
+        int c=1;
+        while (c<=n){
+            System.out.println(c);
+            c++;
+        }
+                
+                
+                
+    }
+    
+}
